e69f7982d155ec33dc93ac2463555d801e5cccf3
SrSiestas15 <munozsev@sheridancollege.ca>
2024-01-10 13:14:25 -0500

    Task 4b completed; turret rotation

---
+++ Assets/Week 1/Scripts/Turret.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Vertical");
+        transform.Rotate(0, 0, speed * direction * Time.deltaTime);
+    }
+}
